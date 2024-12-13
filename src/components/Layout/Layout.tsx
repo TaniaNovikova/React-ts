ed349509dce6a75de347df4d7e09d81d46456213
TaniaNovikova <tania.novikov@web.de>
2024-12-13 18:48:46 +0100

    Lesson12

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,31 @@
+
+import { Footer, Header, LayoutComponent, NavContainer, LogoContainer, Main, StyledNavLink } from "./styles"
+import { LayoutProps } from "./types"
+
+function Layout({ children }: LayoutProps) {
+  return (
+    <LayoutComponent>
+      <Header>
+        <LogoContainer></LogoContainer>
+        <NavContainer>
+          <StyledNavLink to='/' style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>
+            Home
+          </StyledNavLink>
+          <StyledNavLink to='/about' style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>
+            About
+          </StyledNavLink>
+          <StyledNavLink to='/users' style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>
+            Users
+          </StyledNavLink>
+        </NavContainer>
+      </Header>
+      <Main>
+        {/* вместо prop children происходит подстановка различного контента в Layout при изменении url */}
+        {children}
+      </Main>
+      <Footer><LogoContainer></LogoContainer></Footer>
+    </LayoutComponent>
+  )
+}
+
+export default Layout
