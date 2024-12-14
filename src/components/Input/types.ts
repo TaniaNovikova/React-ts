4fe95649e66fb1033f3055ade1213440d9a2799d
TaniaNovikova <tania.novikov@web.de>
2024-12-14 17:44:36 +0100

    add type Number to Input

--- src/components/Input/types.ts
+++ src/components/Input/types.ts
@@ -1,13 +1,14 @@
 import { ChangeEvent } from "react";
 
+
 export interface InputProps {
-  name: string,
-  id?: string,
-  type?: 'text' | 'email' | 'password' | 'tel',
-  placeholder?: string,
-  label?: string,
-  // временно делаем пропсы value и onChange необязательными, чтобы не было ошибок в старых компонентах
-  value?: string,
-  onChange?: (event: ChangeEvent<HTMLInputElement>) => void
-  error?: string;
+  name: string;
+  id?: string;
+  type?: "text" | "email" | "password" | "tel" | "number";
+  placeholder?: string;
+  label?: string;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  value?: string | number;
+  min?: number;
+  step?: number;
 }
