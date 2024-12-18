f924a8b8de0462a46f53f00ae6920ef7c10e3466
TaniaNovikova <tania.novikov@web.de>
2024-12-18 17:53:57 +0100

    fix import for Meta

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from 'components/Layout/Layout';
-import GlobalStyles from './styles/GlobalStyles';
-import About from 'pages/About/About';
-import Home from 'pages/Home/Home';
-import Users from 'pages/Users/Users';
+import Layout from "components/Layout/Layout";
+import GlobalStyles from "./styles/GlobalStyles";
+import About from "pages/About/About";
+import Home from "pages/Home/Home";
+import Users from "pages/Users/Users";
+import Clients from "pages/Clients/Clients";
+import SAP from "pages/Clients/components/SAP/SAP";
+import Oracle from "pages/Clients/components/Oracle/Oracle";
+import Meta from "pages/Clients/components/Meta/Meta";
 
 //lessons
 // import Lesson06 from './lessons/Lesson06/Lesson06';
@@ -19,23 +23,27 @@ import Users from 'pages/Users/Users';
 // import Homework09 from 'homeworks/Homework09/Homework09';
 // import Homework11 from 'homeworks/Homework11/Homework11';
 
+
 //consultations
 // import Consultation04 from './consultations/Consultation04/Consultation04';
 // import Consultation05 from 'consultations/Consultation05/Consultation05';
-
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
-      {/* <Layout>
+       <Layout>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/users' element={<Users />} />
-          <Route path='*' element='Page not found' />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/clients" element={<Clients />} />
+          <Route path="/sap" element={<SAP />} />
+          <Route path="/meta" element={<Meta />} />
+          <Route path="/oracle" element={<Oracle />} />
+          <Route path="*" element="Page not found" />
         </Routes>
-      </Layout> */}
+      </Layout>
       {/* <Lesson06 /> */}
       {/* <Lesson07 /> */}
       {/* <Consultation04 /> */}
